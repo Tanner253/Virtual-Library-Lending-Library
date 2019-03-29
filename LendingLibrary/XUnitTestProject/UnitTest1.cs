bafe2b9b762a5944bba83113f91e2185ca5c2792
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-28 19:48:01 -0700

    will finish pr in morning

--- LendingLibrary/XUnitTestProject/UnitTest1.cs
+++ LendingLibrary/XUnitTestProject/UnitTest1.cs
@@ -1,12 +1,14 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace XUnitTestProject
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanReturnCount()
         {
 
         }
