b9c5908bd896fc915af0ef42bafa31e4c9a33f1c
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-28 19:23:58 -0700

    huge build out day for me forgot to do alot of commits

---
+++ LendingLibrary/LendingLibrary/classes/Library.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using LendingLibrary.classes;
+
+namespace LendingLibrary.classes
+{
+    class Library<T> : IEnumerable
+    {
+        T[] books = new T[10];
+        int count = 0;
+        public void Add(T book)
+        {
+            if(count == books.Length)
+            {
+                Array.Resize(ref books, (books.Length * 2));
+            }
+            books[count++] = book;
+        }
+
+        //public void Remove(T number)
+        //see demo for start and finish
+
+        /*public void Remove(string title, Library<Book> library)
+        {
+            for (int i = 0; i < library.Count(); i++)
+            {
+                if(title == library[i])
+                {
+
+                }
+            }
+        }*/
+        public int Count()
+        {
+            return count;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+    }
+}
